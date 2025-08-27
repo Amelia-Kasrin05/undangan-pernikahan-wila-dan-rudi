b2bc3704bdce495a5e334e9b04813e101a7a0ea6
Your Name <ameliakasrin05@gmail.com>
2025-08-27 15:18:14 +0700

    bunga hampir fix

--- src/templates/components/mainLayout.tsx
+++ src/templates/components/mainLayout.tsx
@@ -16,11 +16,27 @@ export default function MainLayout({
     <section className={`relative w-full ${height} flex flex-col justify-center items-center ${backgroundColor} p-32 px-10 ${className}`}>
       {flower && (
         <div className={`w-full h-full overflow-hidden absolute z-0`}>
-          {/* Ornamen horizontal di bagian atas */}
-          <img src="/images/bunga3.png" alt="" className="absolute top-4 left-1/2 transform -translate-x-1/2 w-full max-w-2xl h-16 object-contain z-0" />
+          {/* Ornamen horizontal di bagian atas - memanjang penuh dari kiri ke kanan */}
+          <img
+            src="/images/bunga3.png"
+            alt=""
+            className="absolute top-0 left-0 w-full h-20 object-cover object-center z-0"
+            style={{
+              objectFit: "cover",
+              objectPosition: "center",
+            }}
+          />
 
-          {/* Ornamen horizontal di bagian bawah */}
-          <img src="/images/bunga3.png" alt="" className="absolute bottom-4 left-1/2 transform -translate-x-1/2 rotate-180 w-full max-w-2xl h-16 object-contain z-0" />
+          {/* Ornamen horizontal di bagian bawah - memanjang penuh dari kiri ke kanan */}
+          <img
+            src="/images/bunga3.png"
+            alt=""
+            className="absolute bottom-0 left-0 rotate-180 w-full h-20 object-cover object-center z-0"
+            style={{
+              objectFit: "cover",
+              objectPosition: "center",
+            }}
+          />
 
           {/* Background bercak tetap dipertahankan */}
           <img src="/images/bercak2.png" alt="" className="absolute -left-56 top-1/2 opacity-20" />
