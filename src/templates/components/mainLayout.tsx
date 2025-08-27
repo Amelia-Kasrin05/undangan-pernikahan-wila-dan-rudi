2c58e596a0382a6df72814fcc822b9dc36494d24
Your Name <ameliakasrin05@gmail.com>
2025-08-27 14:39:38 +0700

    bunga

--- src/templates/components/mainLayout.tsx
+++ src/templates/components/mainLayout.tsx
@@ -1,3 +1,4 @@
+import type React from "react";
 export default function MainLayout({
   children,
   height = "min-h-screen",
@@ -15,13 +16,15 @@ export default function MainLayout({
     <section className={`relative w-full ${height} flex flex-col justify-center items-center ${backgroundColor} p-32 px-10 ${className}`}>
       {flower && (
         <div className={`w-full h-full overflow-hidden absolute z-0`}>
+          {/* Ornamen horizontal di bagian atas */}
+          <img src="/images/bunga3.png" alt="" className="absolute top-4 left-1/2 transform -translate-x-1/2 w-full max-w-2xl h-16 object-contain z-0" />
+
+          {/* Ornamen horizontal di bagian bawah */}
+          <img src="/images/bunga3.png" alt="" className="absolute bottom-4 left-1/2 transform -translate-x-1/2 rotate-180 w-full max-w-2xl h-16 object-contain z-0" />
+
+          {/* Background bercak tetap dipertahankan */}
           <img src="/images/bercak2.png" alt="" className="absolute -left-56 top-1/2 opacity-20" />
           <img src="/images/bercak2.png" alt="" className="absolute -right-48 top-[15%] opacity-20" />
-          <img src="/images/bunga3.png" alt="" className="absolute -top-9 -left-5 w-[60dvw] max-w-64 z-0" />
-          <img src="/images/bunga4.png" alt="" className="absolute -top-[8rem] -right-10 w-[60dvw] max-w-64 z-0 rotate-[110deg]" />
-
-          <img src="/images/bunga3.png" alt="" className="absolute -bottom-9 -right-5 w-[60dvw] max-w-64 z-0 rotate-180" />
-          <img src="/images/bunga4.png" alt="" className="absolute -bottom-[7.5rem] -left-10 w-[60dvw] max-w-64 z-0 rotate-[-65deg]" />
         </div>
       )}
       <div className={`w-full h-full -top-10 overflow-hidden absolute z-0`}></div>
